786a7d538adbeada1e594fba9474ad7f954a45b1
BreyneWard <wardbreyne@hotmail.com>
2022-12-06 20:19:42 +0100

    6.6 . HogerLager

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _6._6_hogerlager;
+class Program
+{
+    static void Main(string[] args)
+    {
+         
+        Random orakel = new Random();
+        int getal = orakel.Next(1, 101);
+        int gok = 0;
+        int pogingen = 0;
+
+        do
+        {
+            if(pogingen != 10)
+            {
+                Console.WriteLine($"Raad het getal van 1 t.e.m. 100. (10 pogingen):");
+                gok = int.Parse(Console.ReadLine());
+                if (gok > getal)
+                {
+                    Console.WriteLine("Fout getal: Getal is lager.");
+                    pogingen++;
+                }
+                else if (gok < getal)
+                {
+                    Console.WriteLine("Fout getal: Getal is hoger.");
+                    pogingen++;
+                }
+                else
+                {
+                    pogingen++;
+                    Console.WriteLine($"Gewonnen in {pogingen} pogingen.");
+                }      
+            }
+            else
+            {
+                Console.WriteLine($"Verloren: max aantal pogingen bereikt.");
+                break;
+            }
+        }while (getal != gok);    
+    }
+}
